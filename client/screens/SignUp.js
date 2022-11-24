73e2dd8a1729beb04241a757eca51d4eeea392b7
srushteeM <srushteemarathe@gmail.com>
2022-11-25 02:14:00 +0530

    Created SignUp UI

--- client/screens/SignUp.js
+++ client/screens/SignUp.js
@@ -1,20 +1,135 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert,
+} from "react-native";
 
-class SignUp extends Component {
+export default class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
     };
   }
 
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed " + viewId);
+  };
+
   render() {
     return (
-      <View>
-        <Text> SignUp </Text>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputs}
+            placeholder="Enter your name"
+            underlineColorAndroid="transparent"
+            onChangeText={(name) => this.setState({ username: name })}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          {/* <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/> */}
+          <TextInput
+            style={styles.inputs}
+            placeholder="Email"
+            keyboardType="email-address"
+            underlineColorAndroid="transparent"
+            onChangeText={(email) => this.setState({ email: email })}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          {/* <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/> */}
+          <TextInput
+            style={styles.inputs}
+            placeholder="Password"
+            secureTextEntry={true}
+            underlineColorAndroid="transparent"
+            onChangeText={(password) => this.setState({ password: password })}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.inputs}
+            placeholder="Confirm Password"
+            underlineColorAndroid="transparent"
+            onChangeText={(confirmPassword) =>
+              this.setState({ confirmPassword: confirmPassword })
+            }
+          />
+        </View>
+        <TouchableHighlight
+          style={[styles.buttonContainer, styles.loginButton]}
+          onPress={() => this.onClickListener("login")}
+        >
+          <Text style={styles.loginText}>SignUp</Text>
+        </TouchableHighlight>
+
+       
+
+        <TouchableHighlight
+          style={styles.buttonContainer}
+          onPress={() => this.onClickListener("register")}
+        >
+          <Text>Already have an account!</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
-export default SignUp;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#DCDCDC",
+  },
+  inputContainer: {
+    borderBottomColor: "#F5FCFF",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: "#FFFFFF",
+    flex: 1,
+  },
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+  },
+  loginButton: {
+    backgroundColor: "#00b5ec",
+  },
+  loginText: {
+    color: "white",
+  },
+});
