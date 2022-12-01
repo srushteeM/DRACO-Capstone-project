cef9edddf4ee8aeeaa91574a75b83f0c594d7ed6
srushteeM <srushteemarathe@gmail.com>
2022-12-01 16:35:21 +0530

    added ProfileScreen component

--- client/App.js
+++ client/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import Login from './screens/Login'
 import SignUp from './screens/SignUp'
+import ProfileScreen from './screens/ProfileScreen'
 export default class APP extends Component {
   render() {
     return (
-     <SignUp/>
+     <ProfileScreen/>
     )
   }
 }
