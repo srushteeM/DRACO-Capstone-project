958ca8d8ed339219dd69deb5b797c4020f44b1c8
srushteeM <srushteemarathe@gmail.com>
2022-12-01 18:13:21 +0530

    restructured profile page acoording to changed design

--- client/screens/ProfileScreen.js
+++ client/screens/ProfileScreen.js
@@ -1,59 +1,126 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+} from "react-native";
 
-export default class UserProfileView extends Component {
+export default class ProfileScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profileImage: "",
+      username: "",
+      aboutMe: "",
+      phone: 0,
+      email: "",
+    };
+  }
+  // Function to fetch user information from database
+  fetchUserData = () => {};
+  componentDidMount() {
+    this.fetchUserData();
+  }
+
+  // Function to change user information in database
+  updateUserData = (info_type, info) => {};
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
           <View style={styles.headerContent}>
+            {/* Button to navigate to home screen */}
+            <TouchableHighlight>
             <Image
-              style={styles.avatar}
-              source={{
-                uri: "https://bootdey.com/img/Content/avatar/avatar6.png",
-              }}
-            />
+                  style={styles.icon}
+                  source={{
+                    uri: "https://www.pngfind.com/pngs/m/73-734715_left-arrow-png-back-arrow-icon-android-transparent.png",
+                  }}/>
+            </TouchableHighlight>
+            {/* Profile picture */}
+            <View>
+              <Image
+                style={styles.avatar}
+                source={{
+                  uri: "https://bootdey.com/img/Content/avatar/avatar6.png",
+                }}
+              />
+              <TouchableHighlight>
+                <Image
+                  style={styles.icon}
+                  source={{
+                    uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwaZahYMhgJM47ENIg34mIKavn1ve1ivGCXGYK5YbdRWiCk65ReUVB_DeF8P07GBC4Dl4&usqp=CAU",
+                  }}
+                />
+              </TouchableHighlight>
 
-            <Text style={styles.name}>John Doe </Text>
-            <Text style={styles.userInfo}>jhonnydoe@mail.com </Text>
-            <Text style={styles.userInfo}>Florida </Text>
+              <Text style={styles.name}>John Doe </Text>
+            </View>
           </View>
         </View>
 
         <View style={styles.body}>
+          {/* Name */}
           <View style={styles.item}>
             <View style={styles.iconContent}>
               <Image style={styles.icon} source={{ uri: "#" }} />
             </View>
             <View style={styles.infoContent}>
-              <Text style={styles.info}>Name</Text>
+              <Text style={styles.info}>Name:{this.state.name}</Text>
             </View>
+            <TouchableHighlight>
+              <Image
+                style={styles.icon}
+                source={{
+                  uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwaZahYMhgJM47ENIg34mIKavn1ve1ivGCXGYK5YbdRWiCk65ReUVB_DeF8P07GBC4Dl4&usqp=CAU",
+                }}
+              />
+            </TouchableHighlight>
           </View>
-
+          {/* About */}
           <View style={styles.item}>
             <View style={styles.iconContent}>
               <Image style={styles.icon} source={{ uri: "#" }} />
             </View>
             <View style={styles.infoContent}>
-              <Text style={styles.info}>About</Text>
+              <Text style={styles.info}>About Me:{this.state.aboutMe}</Text>
             </View>
+            <TouchableHighlight>
+              <Image
+                style={styles.icon}
+                source={{
+                  uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwaZahYMhgJM47ENIg34mIKavn1ve1ivGCXGYK5YbdRWiCk65ReUVB_DeF8P07GBC4Dl4&usqp=CAU",
+                }}
+              />
+            </TouchableHighlight>
           </View>
-
+          {/* Phone number */}
           <View style={styles.item}>
             <View style={styles.iconContent}>
               <Image style={styles.icon} source={{ uri: "#" }} />
             </View>
             <View style={styles.infoContent}>
-              <Text style={styles.info}>Home</Text>
+              <Text style={styles.info}>Phone:{this.state.phone}</Text>
             </View>
+            <TouchableHighlight>
+              <Image
+                style={styles.icon}
+                source={{
+                  uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwaZahYMhgJM47ENIg34mIKavn1ve1ivGCXGYK5YbdRWiCk65ReUVB_DeF8P07GBC4Dl4&usqp=CAU",
+                }}
+              />
+            </TouchableHighlight>
           </View>
-
+          {/* Email Address */}
           <View style={styles.item}>
             <View style={styles.iconContent}>
               <Image style={styles.icon} source={{ uri: "#" }} />
             </View>
             <View style={styles.infoContent}>
-              <Text style={styles.info}>Email</Text>
+              <Text style={styles.info}>Email address:{this.state.email}</Text>
             </View>
           </View>
         </View>
@@ -85,11 +152,11 @@ const styles = StyleSheet.create({
   },
   userInfo: {
     fontSize: 16,
-    color: "#778899",
+    color: "#000000",
     fontWeight: "600",
   },
   body: {
-    backgroundColor: "#778899",
+    backgroundColor: "#ffffff",
     height: 500,
     alignItems: "center",
   },
@@ -110,10 +177,11 @@ const styles = StyleSheet.create({
     width: 30,
     height: 30,
     marginTop: 20,
+    marginLeft: 30,
   },
   info: {
     fontSize: 18,
     marginTop: 20,
-    color: "#FFFFFF",
+    color: "#000000",
   },
 });
