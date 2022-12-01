c9dbed3fef86a877aac493a5efced657285594a2
srushteeM <srushteemarathe@gmail.com>
2022-12-01 17:15:32 +0530

    restructured login page acoording to changed design

--- client/screens/Login.js
+++ client/screens/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,54 +7,86 @@ import {
   Button,
   TouchableHighlight,
   Image,
-  Alert
-} from 'react-native';
-
+  Alert,
+} from "react-native";
+//import { Stack, TextInput, IconButton } from "@react-native-material/core";
+//import Icon from "@expo/vector-icons/MaterialCommunityIcons";
+import AppLogo from "../components/AppLogo";
 export default class Login extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      email   : '',
-      password: '',
-    }
+      email: "",
+      password: "",
+    };
   }
 
   onClickListener = (viewId) => {
-    Alert.alert("Alert", "Button pressed "+viewId);
-  }
+    Alert.alert("Alert", "Button pressed " + viewId);
+  };
 
   render() {
     return (
       <View style={styles.container}>
+        {/* Logo of the app */}
+        <AppLogo />
+        <Text>Hi! Please login to your account</Text>
+        {/* Image on the login page */}
+        <Image source={{ uri: "#" }} />
+
+        {/* Email address input */}
         <View style={styles.inputContainer}>
-          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
-          <TextInput style={styles.inputs}
-              placeholder="Email"
-              keyboardType="email-address"
-              underlineColorAndroid='transparent'
-              onChangeText={(email) => this.setState({email:email})}/>
+          <Image
+            style={styles.inputIcon}
+            source={{
+              uri: "https://png.icons8.com/message/ultraviolet/50/3498db",
+            }}
+          />
+          <TextInput
+            style={styles.inputs}
+            placeholder="Email"
+            keyboardType="email-address"
+            underlineColorAndroid="transparent"
+            onChangeText={(email) => this.setState({ email: email })}
+          />
         </View>
-        
+
+        {/* Password address input */}
         <View style={styles.inputContainer}>
-          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
-          <TextInput style={styles.inputs}
-              placeholder="Password"
-              secureTextEntry={true}
-              underlineColorAndroid='transparent'
-              onChangeText={(password) => this.setState({password:password})}/>
+          <Image
+            style={styles.inputIcon}
+            source={{
+              uri: "https://png.icons8.com/key-2/ultraviolet/50/3498db",
+            }}
+          />
+          <TextInput
+            style={styles.inputs}
+            placeholder="Password"
+            secureTextEntry={true}
+            underlineColorAndroid="transparent"
+            onChangeText={(password) => this.setState({ password: password })}
+          />
         </View>
-
-        <TouchableHighlight style={[styles.buttonContainer, styles.loginButton]} onPress={() => this.onClickListener('login')}>
-          <Text style={styles.loginText}>Login</Text>
+        {/* Forgot Password Button */}
+        <TouchableHighlight
+          style={styles.buttonContainer}
+          onPress={() => this.onClickListener("forgot_password")}
+        >
+          <Text >Forgot Password?</Text>
         </TouchableHighlight>
-
-        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('restore_password')}>
-            <Text>Forgot your password?</Text>
+        {/* Login Button */}
+        <TouchableHighlight
+          style={[styles.buttonContainer, styles.loginButton]}
+          onPress={() => this.onClickListener("login")}
+        >
+          <Text>Login</Text>
         </TouchableHighlight>
-
-        <TouchableHighlight style={styles.buttonContainer} onPress={() => this.onClickListener('register')}>
-            <Text>SignUp</Text>
+        {/* Signup Button */}
+        <TouchableHighlight
+          style={styles.buttonContainer}
+          onPress={() => this.onClickListener("signUp")}
+        >
+          <Text>Don't have an account? SignUp</Text>
         </TouchableHighlight>
       </View>
     );
@@ -64,46 +96,46 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#DCDCDC',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#DCDCDC",
   },
   inputContainer: {
-      borderBottomColor: '#F5FCFF',
-      backgroundColor: '#FFFFFF',
-      borderRadius:30,
-      borderBottomWidth: 1,
-      width:250,
-      height:45,
-      marginBottom:20,
-      flexDirection: 'row',
-      alignItems:'center'
+    borderBottomColor: "#F5FCFF",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    borderBottomWidth: 1,
+    width: 250,
+    height: 45,
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
   },
-  inputs:{
-      height:45,
-      marginLeft:16,
-      borderBottomColor: '#FFFFFF',
-      flex:1,
+  inputs: {
+    height: 45,
+    marginLeft: 16,
+    borderBottomColor: "#FFFFFF",
+    flex: 1,
   },
-  inputIcon:{
-    width:30,
-    height:30,
-    marginLeft:15,
-    justifyContent: 'center'
+  inputIcon: {
+    width: 30,
+    height: 30,
+    marginLeft: 15,
+    justifyContent: "center",
   },
   buttonContainer: {
-    height:45,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
+    height: 45,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
   },
   loginButton: {
     backgroundColor: "#00b5ec",
   },
   loginText: {
-    color: 'white',
-  }
+    color: "white",
+  },
 });
