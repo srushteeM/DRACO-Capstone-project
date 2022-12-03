016187c26f3f9ee9b203384ede5807f1f7fd8a6b
srushteeM <59560681+srushteeM@users.noreply.github.com>
2022-12-04 00:42:12 +0530

    Signup & signin screen UI part has been completed (#1)
    Co-authored-by: prashant soni <prashantsoni.skg@gmail.com>

--- client/App.js
+++ client/App.js
@@ -3,10 +3,15 @@ import { Text, View } from 'react-native'
 import Login from './screens/Login'
 import SignUp from './screens/SignUp'
 import ProfileScreen from './screens/ProfileScreen'
+
 export default class APP extends Component {
   render() {
     return (
-     <ProfileScreen/>
+      <>
+        {/* <SignUp /> */}
+        <Login />
+        {/* <ProfileScreen /> */}
+      </>
     )
   }
 }
