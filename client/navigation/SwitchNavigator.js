181e98ba4ecd1257e01fe56a9c60559d9ae46615
prashant soni <prashantsoni.skg@gmail.com>
2022-12-08 21:26:13 +0530

    update navigation

--- client/navigation/SwitchNavigator.js
+++ client/navigation/SwitchNavigator.js
@@ -1,11 +1,19 @@
-import {createSwitchNavigator } from "react-navigation";
+// import {createSwitchNavigator } from "react-navigation";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import TopTabNavigator from './TopTabNavigator';
-import Login from '../screens/Login'
-import SignUp from '../screens/SignUp'
-const SwitchNavigator = createSwitchNavigator({
-    Login: { screen: Login },
-    SignUp: { screen: SignUp },
-    TopTabNavigator: { screen: TopTabNavigator }
-  });
-  
-  export default SwitchNavigator;
+import Login from '../screens/Login';
+import SignUp from '../screens/SignUp';
+const Stack = createNativeStackNavigator();
+
+function SwitchNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="TopTabNavigator" component={TopTabNavigator} />
+    </Stack.Navigator>
+  );
+}
+
+export default SwitchNavigator
