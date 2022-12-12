deff0288b8324fd35e97bd99de984b0ee7ac6003
srushteeM <srushteemarathe@gmail.com>
2022-12-12 22:33:42 +0530

    fixed issue with firebase and made login and signup screen functional

--- client/screens/Login.js
+++ client/screens/Login.js
@@ -15,7 +15,7 @@ export default class Login extends Component {
       password: "",
     };
   }
-  userLogin = (emailId, password) => {
+  userLogin = (email, password) => {
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
