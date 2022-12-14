de06e94e00104d89edb239ff0097f141cbb8bf75
srushteeM <srushteemarathe@gmail.com>
2022-12-14 15:25:06 +0530

    stories

--- client/screens/StoriesScreen.js
+++ client/screens/StoriesScreen.js
@@ -10,6 +10,8 @@ import {
   TouchableHighlight,
   FlatList,
 } from "react-native";
+import firebase from "firebase";
+import db from "../config"
 
 class StoriesScreen extends Component {
   constructor(props) {
@@ -19,35 +21,44 @@ class StoriesScreen extends Component {
     };
   }
   fetchPosts = async () => {
-    const data = [
-      {
-        id: 1,
-        username: "Planet of Nature",
-        profileImage:
-          "https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        postImage:
-          "https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        postMessage:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-      },
-      {
-        id: 2,
-        username: "Lampost",
-        profileImage:
-          "https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        postImage:
-          "https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-        postMessage:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
-      },
-    ];
-    this.setState({ data: data });
+    // const data = [
+    //   {
+    //     id: 1,
+    //     username: "Planet of Nature",
+    //     profileImage:
+    //       "https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    //     postImage:
+    //       "https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    //     postMessage:
+    //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    //   },
+    //   {
+    //     id: 2,
+    //     username: "Lampost",
+    //     profileImage:
+    //       "https://images.unsplash.com/photo-1559526323-cb2f2fe2591b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    //     postImage:
+    //       "https://images.unsplash.com/photo-1482822683622-00effad5052e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+    //     postMessage:
+    //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+    //   },
+    // ];
+    // this.setState({ data: data });
+    this.requestRef = db
+    .collection("stories")
+    .onSnapshot((snapshot) => {
+      var stories = snapshot.docs.map((doc) => doc.data());
+      this.setState({
+        data: stories,
+      });
+    });
+    console.log(this.state.data);
   };
   componentDidMount() {
     this.fetchPosts();
   }
-  renderItem = ({ item }) => (
-    <View style={styles.card}>
+  renderItem = ({ item,key }) => (
+    <View style={styles.card} key={this.state.unique_id}>
       <Image src={{ uri: item.profileImage }} />
       <Text style={styles.title}>{item.username}</Text>
       <Image src={{ uri: item.postImage }} style={styles.cardImage} />
@@ -68,7 +79,7 @@ class StoriesScreen extends Component {
               style={styles.list}
               data={this.state.data}
               renderItem={this.renderItem}
-              keyExtractor={(item) => item.id}
+              keyExtractor={(item) => item.unique_id}
               ItemSeparatorComponent={() => {
                 return <View style={styles.separator} />;
               }}
