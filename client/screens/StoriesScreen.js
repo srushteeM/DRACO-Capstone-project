57a5417d8fdbb8280b101cb2cb6fea0f488a4dc7
prashant soni <prashantsoni.skg@gmail.com>
2022-12-15 03:36:06 +0530

    create post styling done

--- client/screens/StoriesScreen.js
+++ client/screens/StoriesScreen.js
@@ -3,9 +3,14 @@ import {
   StyleSheet,
   ActivityIndicator,
   TouchableHighlight,
-  FlatList,
+  Text,
   View,
-  Text,Image
+  TouchableOpacity,
+  Image,
+  Alert,
+  ScrollView,
+  TextInput,
+  FlatList
 } from "react-native";
 import firebase from "firebase";
 import db from "../config"
@@ -14,9 +19,67 @@ export default class StoriesScreen extends Component {
     super(props);
     this.state = {
       data: [
-        
+        {id:1, color:"#FF4500", icon:"https://bootdey.com/img/Content/avatar/avatar1.png", name: "User 1", tags:['tag 1', 'tag 2', 'tag 3', 'Mobile dev', 'RN', 'Bootdey']},
+        {id:2, color:"#87CEEB", icon:"https://bootdey.com/img/Content/avatar/avatar2.png", name: "User 2", tags:['tag 1', 'tag 2', 'tag 3', 'Dey-Dey', 'Developer']}, 
+        {id:3, color:"#4682B4", icon:"https://bootdey.com/img/Content/avatar/avatar3.png", name: "User 3", tags:['tag 1', 'tag 2', 'tag 3']}, 
+        {id:4, color:"#6A5ACD", icon:"https://bootdey.com/img/Content/avatar/avatar4.png", name: "User 4", tags:['tag 1', 'tag 2', 'tag 3']},   
       ],
     };
+  }
+
+  cardClickEventListener = (item) => {
+    Alert.alert(item.name);
+  }
+
+  tagClickEventListener = (tagName) => {
+    Alert.alert(tagName);
+  }
+
+  renderTags = (item) =>{
+    return item.tags.map((tag, key) => {
+      return (
+        <TouchableOpacity key={key} style={styles.btnColor} onPress={() => {this.tagClickEventListener(tag)}}>
+          <Text>{tag}</Text>
+        </TouchableOpacity> 
+      );
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.formContent}>
+          <View style={styles.inputContainer}>
+            <Image style={[styles.icon, styles.inputIcon]} source={{uri: 'https://png.icons8.com/search/androidL/100/000000'}}/>
+            <TextInput style={styles.inputs}
+              ref={'txtSearch'}
+              placeholder="Search"
+              underlineColorAndroid='transparent'
+              onChangeText={(name_address) => this.setState({name_address})}/>
+          </View>
+        </View>
+
+        <FlatList 
+          style={styles.notificationList}
+          data={this.state.data}
+          keyExtractor= {(item) => {
+            return item.id;
+          }}
+          renderItem={({item}) => {
+            return (
+              <TouchableOpacity style={[styles.card, {borderColor:item.color}]} onPress={() => {this.cardClickEventListener(item)}}>
+                <View style={styles.cardContent}>
+                  <Image style={[styles.image, styles.imageContent]} source={{uri: item.icon}}/>
+                  <Text style={styles.name}>{item.name}</Text>
+                </View>
+                <View style={[styles.cardContent, styles.tagsContent]}>
+                  {this.renderTags(item)}
+                </View>
+              </TouchableOpacity>
+            )
+          }}/>
+      </View>
+    );
   }
 
   fetchPosts = async () => {
@@ -58,7 +121,7 @@ export default class StoriesScreen extends Component {
           <TouchableHighlight
             onPress={() => this.props.navigation.navigate("Create Post")}
           >
-            <Text>Create Post...</Text>
+            <Text style={styles.createPostBtn} >Create New Post</Text>
           </TouchableHighlight>
           <View style={styles.container}>
             <FlatList
@@ -94,12 +157,12 @@ const styles = StyleSheet.create({
   card:{
     shadowColor: '#00000021',
     shadowOffset: {
-      width: 2
+      width: 2, 
     },
     shadowOpacity: 0.5,
     shadowRadius: 4,
     marginVertical: 8,
-    backgroundColor:"white"
+    backgroundColor:"#fff",                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
   },
   cardHeader: {
     paddingVertical: 17,
@@ -128,7 +191,9 @@ const styles = StyleSheet.create({
     height: 150,
     width: null,
   },
+
   /******** card components **************/
+
   title:{
     fontSize:18,
     flex:1,
@@ -158,7 +223,9 @@ const styles = StyleSheet.create({
   timeContainer:{
     flexDirection:'row'
   },
+
   /******** social bar ******************/
+  
   socialBarContainer: {
     justifyContent: 'center',
     alignItems: 'center',
@@ -179,87 +246,20 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+
+  createPostBtn:{
+      backgroundColor:'#8530d1',
+      color:'#fff',
+      textAlign:'center',
+      lineHeight:'px',
+      margin:'12px',
+      borderTopLeftRadius:'4px',
+      borderBottomRightRadius:'4px',
+      borderBottomLeftRadius:'4px',
+      borderTopRightRadius:'4px',
+      borderTopLeftRadius:'4px',
+      padding:'5px'
   }
-});  
-                  
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#EBEBEB',
-//   },
-//   formContent:{
-//     flexDirection: 'row',
-//     marginTop:30,
-//   },
-//   inputContainer: {
-//     borderBottomColor: '#F5FCFF',
-//     backgroundColor: '#FFFFFF',
-//     borderRadius:30,
-//     borderBottomWidth: 1,
-//     height:45,
-//     flexDirection: 'row',
-//     alignItems:'center',
-//     flex:1,
-//     margin:10,
-//   },
-//   icon:{
-//     width:30,
-//     height:30,
-//   },
-//   iconBtnSearch:{
-//     alignSelf:'center'
-//   },
-//   inputs:{
-//     height:45,
-//     marginLeft:16,
-//     borderBottomColor: '#FFFFFF',
-//     flex:1,
-//   },
-//   inputIcon:{
-//     marginLeft:15,
-//     justifyContent: 'center'
-//   },
-//   notificationList:{
-//     marginTop:20,
-//     padding:10,
-//   },
-//   card: {
-//     height:null,
-//     paddingTop:10,
-//     paddingBottom:10,
-//     marginTop:5,
-//     backgroundColor: '#FFFFFF',
-//     flexDirection: 'column',
-//     borderTopWidth:40,
-//     marginBottom:20,
-//   },
-//   cardContent:{
-//     flexDirection:'row',
-//     marginLeft:10, 
-//   },
-//   imageContent:{
-//     marginTop:-40,
-//   },
-//   tagsContent:{
-//     marginTop:10,
-//     flexWrap:'wrap'
-//   },
-//   image:{
-//     width:60,
-//     height:60,
-//     borderRadius:30,
-//   },
-//   name:{
-//     fontSize:20,
-//     fontWeight: 'bold',
-//     marginLeft:10,
-//     alignSelf: 'center'
-//   },
-//   btnColor: {
-//     padding:10,
-//     borderRadius:40,
-//     marginHorizontal:3,
-//     backgroundColor: "#eee",
-//     marginTop:5,
-//   },
-// });  
+
+});    
